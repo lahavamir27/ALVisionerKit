8183f724309eaeb614ed73b038181ef44321191d
amir lahav <lahavamir27@gmail.com>
2019-11-27 14:20:37 +0200

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/ALVisionerKit/ALVisionerKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ALVisionerKit : NSObject
+@end
+@implementation PodsDummy_ALVisionerKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-ALVisionerKit_Example/Pods-ALVisionerKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALVisionerKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ALVisionerKit_Example
+@end
